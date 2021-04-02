c0ebc8e76bb59fa9a2f6c9e02ab94d25f5b1b38e
BanishDeveloper <77082028+BanishDeveloper@users.noreply.github.com>
2021-04-02 19:30:34 -0400

    Update EasyModule.lua

--- EasyModule.lua
+++ EasyModule.lua
@@ -4,41 +4,41 @@ local pathfindingService = game:GetService("PathfindingService")
 
 
 getClosestObject = function(objs ,position, lowest, name) -- positions are vectors
-local object = nil
-for _, o in pairs(objs:GetChildren())do
-if o:IsA("BasePart") then
-	local distance = (o.Position - position).Magnitude
-	if distance < lowest then
-		lowest = distance
-		object = o
-	elseif o:IsA("Model") then
-		if o.PrimaryPart then
-			local distance = (o.PrimaryPart.Position - position).Magnitude
+	local object = nil
+	for _, o in pairs(objs:GetChildren())do
+		if o:IsA("BasePart") then
+			local distance = (o.Position - position).Magnitude
 			if distance < lowest then
 				lowest = distance
 				object = o
-			else
-				local distance = (o[name].Position - position).Magnitude
-				if distance < lowest then
-					lowest = distance
-					object = o
-				end
+			elseif o:IsA("Model") then
+				if o.PrimaryPart then
+					local distance = (o.PrimaryPart.Position - position).Magnitude
+					if distance < lowest then
+						lowest = distance
+						object = o
+					else
+						local distance = (o[name].Position - position).Magnitude
+						if distance < lowest then
+							lowest = distance
+							object = o
+						end
+					end
+				end	
 			end
-		end	
-	end
-	return object
-end
+			return object
+		end
 
-pathfindingModule.createPath = function(pos1, pos2) -- note the positions are Vector3 values
-	local point = nil
-	local path = pathfindingService:CreatePath()
+		pathfindingModule.createPath = function(pos1, pos2) -- note the positions are Vector3 values
+			local point = nil
+			local path = pathfindingService:CreatePath()
 
-	-- Compute the path
-	path:ComputeAsync(pos1,pos2)
-	for _, waypoint in pairs(path:GetWaypoints())do
-		point = waypoint
-	end
-	return point -- returns it as a variable
+			-- Compute the path
+			path:ComputeAsync(pos1,pos2)
+			for _, waypoint in pairs(path:GetWaypoints())do
+				point = waypoint
+			end
+		return point -- returns it as a variable
 end
 
 return pathfindingModule
