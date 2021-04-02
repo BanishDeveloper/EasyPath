59b3ded0190e770c5075063d1c6d7b56f19a43a8
BanishDeveloper <77082028+BanishDeveloper@users.noreply.github.com>
2021-04-02 19:28:03 -0400

    Update EasyModule.lua

--- EasyModule.lua
+++ EasyModule.lua
@@ -1,22 +1,4 @@
 pathfindingModule = {} --  <-- module
-local function getClosest(sPos,objs,maxDist) -- starting position (sPos) is a Vector3 value
-	local dist
-	local closestObj
-	if maxDist == nil then
-		dist = math.huge -- huge number
-	else
-		dist = maxDist
-	end
-	for _, o in pairs(objs)do -- gets all objects
-		if (sPos - o.Position).Magnitude > dist then
-			dist = (sPos - o.Position).Magitude
-			closestObj = o	
-		end
-		wait(-.1) -- you can delete this if you want
-	end
-	return closestObj
-end
-
 local pathfindingService = game:GetService("PathfindingService")
 
 
